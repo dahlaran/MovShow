47a7cf901ac6891d52457f3ff18d68c34f24e958
dahlaran <joseph.demerss@hotmail.fr>
2020-12-22 16:34:21 +0100

    Hilt dependency injection set up

--- app/src/main/java/com/dahlaran/movshow/data/TVMazeRepository.kt
+++ app/src/main/java/com/dahlaran/movshow/data/TVMazeRepository.kt
@@ -1,33 +1,35 @@
 package com.dahlaran.movshow.data
 
 import android.util.LruCache
-import com.dahlaran.movshow.data.tvMazeAPI.TVMazeApiServiceBuilder
+import com.dahlaran.movshow.data.tvMazeAPI.TVMazeApiServices
 import com.dahlaran.movshow.models.Show
 import com.dahlaran.movshow.models.TVMazeMedia
 import io.reactivex.Flowable
 import io.reactivex.Observable
+import javax.inject.Inject
+import javax.inject.Singleton
 
-object TVMazeRepository : MediaRepositoryInterface {
-    private const val MAX_ENTRIES: Int = 20
-    private val service = TVMazeApiServiceBuilder.buildService()
+@Singleton
+class TVMazeRepository @Inject constructor(private var service: TVMazeApiServices) : MediaRepositoryInterface{
+    companion object {
+        private const val MAX_ENTRIES: Int = 30
+    }
 
     private val searchCache: LruCache<String, List<TVMazeMedia>> by lazy {
         val cache = LruCache<String, List<TVMazeMedia>>(MAX_ENTRIES)
-        cache.put("",listOf())
+        cache.put("", listOf())
         cache
     }
-    override fun getMedias(): Flowable<List<TVMazeMedia>> {
+
+    override suspend fun getMedias(): Flowable<List<TVMazeMedia>> {
         TODO("Not yet implemented")
     }
 
-
-    override fun searchMediaByTitle(title: String): Observable<List<TVMazeMedia>> {
-        return service.searchMediaByTitle(title).doOnNext {  searchCache.put(title, it) }
+    override suspend fun searchMediaByTitle(title: String): Observable<List<TVMazeMedia>> {
+        return service.searchMediaByTitle(title).doOnNext { searchCache.put(title, it) }
     }
 
-    override fun searchMediaById(id: String): Observable<Show> {
+    override suspend fun searchMediaById(id: String): Observable<Show> {
         return service.searchMediaById(id)
     }
-
-
 }
